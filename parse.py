aedfa4caca7adbc151c78e5b906d773c6bf8aa14
Tolgahan AKGUN <akguntolgahan@gmail.com>
2018-07-15 12:35:51 +0300

    Added send over socket functionality

--- parse.py
+++ parse.py
@@ -3,6 +3,9 @@ import binascii
 import struct
 import sys
 import argparse
+import socket
+import time
+import os
 from pathlib import Path
 from colorama import Fore, Style, init, Back
 
@@ -11,14 +14,32 @@ init(autoreset=True)
 # default endian
 byte_order = 'big'
 
+use_socket = False
+
 def print_red(s):
     print(Fore.RED+str(s)+Style.RESET_ALL)
 
+def send_over_socket(data: bytes):
+    # check ip address and port
+    if (args.ip_address == None) ^ (args.port_number == None):
+        print_red("please enter both ip address and port")
+    else:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((args.ip_address, int(args.port_number)))
+            print("Connected to:"+str(args.ip_address)+":"+str(args.port_number))
+            sock.send(data)
+            print("Data is sent succesfully")
+        except Exception as e:
+            print_red(e)
+        finally:
+            sock.close()
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", type=str, help="file path for parsing the yaml message")
-# TODO send over socket, dump to file and byte order
 parser.add_argument("-ip", "--ip-address", help="ip address the message is sent to, if not given print to stout")
 parser.add_argument("-p", "--port-number", help="port number the message is sent to")
+# TODO send over socket, dump to file and byte order
 parser.add_argument("-out", "--output-file", help="if given the message content will written to the file")
 parser.add_argument("-ord", "--byte-order", help="define big or little endian", choices=['big','little'])
 
@@ -69,5 +90,6 @@ for (key,value) in yaml_object.items():
         buffer.extend(int(value).to_bytes(8, byteorder=byte_order,signed=False))
     elif key == 'float':
         buffer.extend(struct.pack('>f',float(value)))
-
 print (buffer.hex())
+
+send_over_socket(bytes([0x05]))
